1b021165f55eb95d0eb7ce0b3b29b75fbbb3aaaf
xiaowangah <3179806814@qq.com>
2023-03-24 09:03:33 +0800

    lainxi

---
+++ wang.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int imin(int ,int);
+int main(void)
+{
+int l1,l2;
+while(scanf("%d %d",&l1,&l2)==2)
+printf("the kesser of %d and %d is %d\n",l1.l2,imin(li,l2));
+}
+int imin(int n,int m)
+{
+int min;
+if(n<m)
+	min=n;
+else
+	min=m;
+return min;
+
+}
+
